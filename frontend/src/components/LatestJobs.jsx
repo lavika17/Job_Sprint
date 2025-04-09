5f6d09d9ec1027ae785f06fe01de34d8a10a46e6
Lavika Singla <157609834+lavika17@users.noreply.github.com>
2025-04-09 19:44:16 +0530

    Update LatestJobs.jsx

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LatestJobCards from './latestJobCards';
+import LatestJobCards from './LatestJobCards';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 

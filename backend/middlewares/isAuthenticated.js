e8159fbefdb5dca5bec5ef69ba8b947cb1b47aa5
Lavika1703 <lavikasingla1703@gmail.com>
2025-04-10 11:41:05 +0530

    jbe,

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,27 +1,27 @@
 import jwt from "jsonwebtoken";
 const isAuthenticated = async(req , res , next)=>{
     try {
-        // const token = req.cookies.token;
-        // console.log("token: " , token);
+        const token = req.cookies.token;
+        console.log("token: " , token);
         
-        // if(!token){
-        //     return res.status(401).json({
-        //         message: "User not authenticated",
-        //         success:false,
-        //     })
-        // }
-        // const decode = await jwt.verify(token , process.env.SECRET_KEY);
-        // if(!decode){
-        //     return res.status(401).json({
-        //         message:"Invalid token",
-        //         success:false
-        //     })
-        // };
-        // req.id = decode.userId;
+        if(!token){
+            console.log("token: not found" , token);
+            return res.status(401).json({
+                message: "User not authenticated",
+                success:false,
+            })
+        }
+        const decode = await jwt.verify(token , process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        };
+        req.id = decode.userId;
         next();
     } catch (error) {
         console.log(error);      
-        
     }
 }
 export default isAuthenticated;
